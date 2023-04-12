672913133fbe7679ef324577e08d4f7b1af52909
sergey <szaprudnov@hotmail.com>
2023-04-12 21:05:03 +0300

    add class & method

--- heet/working_with_API.py
+++ heet/working_with_API.py
@@ -1,1 +1,9 @@
 """Абстрактный класс для работы с API"""
+
+from abc import ABC, abstractmethod
+
+
+class Working(ABC):
+    @abstractmethod
+    def get_vacancies(self, search_query):
+        pass

77eb1fead160ee65aed52faef335ad76e38e5829
sergey <szaprudnov@hotmail.com>
2023-04-12 21:50:05 +0300

    add class Vacancy & method init

--- heet/vacancies.py
+++ heet/vacancies.py
@@ -1,1 +1,11 @@
 """Класс для работы с вакансиями"""
+
+
+class Vacancy:
+    __slots__ = ('__name', '__url', '__description', '__payment')
+
+    def __init__(self, name: str, url: str, description: str, payment: str):
+        self.__name = name
+        self.__url = url
+        self.__description = description
+        self.__payment = payment

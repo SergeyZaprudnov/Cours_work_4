9e4d30f7ed39b4c453761cce95c7a1b3d4e4cf42
sergey <szaprudnov@hotmail.com>
2023-04-12 21:13:49 +0300

    add class HeadHunterApi

--- heet/hh_api.py
+++ heet/hh_api.py
@@ -1,1 +1,8 @@
 """Работа класса, для работы с ресурсом НН.ru"""
+import requests
+from heet.working_with_API import Working
+
+
+class HeadHunterApi(Working):
+    def __init__(self):
+        self.vacancies = []

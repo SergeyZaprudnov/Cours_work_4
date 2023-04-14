73c4b00a729b1083335397b4f30e3e3fd11670bf
sergey <szaprudnov@hotmail.com>
2023-04-14 19:44:06 +0300

    add class & method init

--- heet/json_class.py
+++ heet/json_class.py
@@ -2,3 +2,8 @@
 import json
 from heet.hh_api import HeadHunterApi
 from heet.save import Save
+
+class JsonSave(Save):
+    def __init__(self, filename):
+        self.filename = filename
+        self.vacancies = []

115cc3b3cbe2e79e113832e5b0266cb49be2bdff
sergey <szaprudnov@hotmail.com>
2023-04-14 19:54:13 +0300

    add method dump_to_file

--- heet/json_class.py
+++ heet/json_class.py
@@ -8,3 +8,7 @@ class JsonSave(Save):
     def __init__(self, filename):
         self.filename = filename
         self.vacancies = []
+
+    def dump_to_file(self, **kwargs):
+        with open("data.json", "w", encoding='utf - 8') as outfile:
+            json.dump(self.filename, outfile, indent=1, ensure_ascii=False)

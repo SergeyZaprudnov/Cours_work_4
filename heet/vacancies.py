ccdf5ad79d99c5f23cf371cd9d3c4d99032cbcd1
sergey <szaprudnov@hotmail.com>
2023-04-18 22:04:09 +0300

    add method str

--- heet/vacancies.py
+++ heet/vacancies.py
@@ -1,8 +1,12 @@
 """Класс для работы с вакансиями"""
 
+
 class Vacancy:
     def __init__(self, title, salary, description, url):
         self.title = title
         self.salary = salary
         self.description = description
         self.url = url
+
+    def __str__(self):
+        return f'title: {self.title}, salary: {self.salary}, description: {self.description}, url: {self.url}'

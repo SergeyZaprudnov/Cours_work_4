418b27f2271cd84e8e3bb27477229789482a911b
sergey <szaprudnov@hotmail.com>
2023-04-18 21:57:32 +0300

    add staticmethod

--- heet/save.py
+++ heet/save.py
@@ -11,3 +11,10 @@ class Save(ABC):
     def words_search(self, words_search):
         pass
 
+    @staticmethod
+    def get_vacancies_salary(self, salary_input):
+        pass
+
+    @staticmethod
+    def save_file(self, resourse=None):
+        pass

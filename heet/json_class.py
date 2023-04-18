c316d9a497c1637d4c03a244b68aa6944f70bb69
sergey <szaprudnov@hotmail.com>
2023-04-18 21:15:13 +0300

    add method save_file

--- heet/json_class.py
+++ heet/json_class.py
@@ -7,3 +7,19 @@ import os
 class JsonSave(Save):
     def __init__(self):
         self.file_name: str = 'JSON.json'
+
+    def save_file(self, headhunter=None, superjob=None):
+        """Сохранение данных с вакансиями"""
+        if headhunter is not None and superjob is None:
+            with open(self.file_name, 'w', encoding='utf-8') as file:
+                json.dump(
+                    sorted([vars(vacancy) for vacancy in headhunter], key=lambda x: x['salary']['from'], reverse=True),
+                    file, ensure_ascii=False, indent=4)
+        elif superjob is not None and headhunter is None:
+            with open(self.file_name, 'w', encoding='utf-8') as file:
+                json.dump(
+                    sorted([vars(vacancy) for vacancy in superjob], key=lambda x: x['salary']['from'], reverse=True),
+                    file, ensure_ascii=False, indent=4)
+        else:
+            print('Нет вакансий')
+

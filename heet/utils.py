ad3462563d02a9b5e64b4b15e3a63846a678bf25
sergey <szaprudnov@hotmail.com>
2023-04-18 22:45:55 +0300

    editing & add method

--- heet/utils.py
+++ heet/utils.py
@@ -5,6 +5,12 @@ from  bs4 import BeautifulSoup
 
 
 
-def get_search_query():
-    query = input("Введите запрос: ")
-    return query
+def user_interaction():
+    """Общение\связь с пользователем"""
+    hh_api, sj_appi = choice_platform()
+    hh_vacancies, sj_vacancies = get_from_platform
+    filter_word_input = filter_words()
+    salary_input = salary_sort()
+    get_result(hh_vacancies, sj_vacancies, filter_word_input, salary_input)
+
+

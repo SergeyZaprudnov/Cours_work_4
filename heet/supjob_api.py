cbcedbd9d5612ddbe57d1bdc8b5a7c1f242e0c7c
sergey <szaprudnov@hotmail.com>
2023-04-19 00:16:02 +0300

    editing

--- heet/supjob_api.py
+++ heet/supjob_api.py
@@ -7,7 +7,7 @@ from heet.vacancies import Vacancy
 
 class SJAPI(Working):
     def __init__(self):
-        self.api_key: str = os.getenv('SJ_API_KEY')
+        self.api_key: str = os.getenv('API_KEY')
         self.headers = {'X-Api-App-ID': self.api_key}
         self.url = 'https://api.superjob.ru/2.0/vacancies/'
 

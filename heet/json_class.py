150636f353cfab99761308840321ef7d5bba2bfc
sergey <szaprudnov@hotmail.com>
2023-04-18 21:02:34 +0300

    del method

--- heet/json_class.py
+++ heet/json_class.py
@@ -7,7 +7,3 @@ import os
 class JsonSave(Save):
     def __init__(self):
         self.file_name: str = 'JSON.json'
-
-    def dump_to_file(self, **kwargs):
-        with open("data.json", "w", encoding='utf - 8') as outfile:
-            json.dump(self.filename, outfile, indent=1, ensure_ascii=False)

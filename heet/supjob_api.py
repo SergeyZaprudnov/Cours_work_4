47df9f5bd3918cba0bcfdd4528f1370a8af20eb2
sergey <szaprudnov@hotmail.com>
2023-04-18 22:23:48 +0300

    add method get_vacancies

--- heet/supjob_api.py
+++ heet/supjob_api.py
@@ -11,3 +11,21 @@ class SJAPI(Working):
         self.headers = {'X-Api-App-ID': self.api_key}
         self.url = 'https://api.superjob.ru/2.0/vacancies/'
 
+    def get_vacancies(self, search_query: str):
+        """Получение вакансий при помощи SJAPI"""
+        options = {'keyword': search_query, 'count': 100}
+        response = requests.get(self.url, headers=self.headers, options=options)
+        if response.status_code == 200:
+            data = response.json()
+            vacancies_data = data['object']
+            vacancies = []
+            for vacancy in vacancies_data:
+                title = vacancy['profession']
+                salary = SJAPI.get_salary(vacancy)
+                description = vacancy['candidat']
+                url = vacancy['link']
+                vacancy = Vacancy(title, salary, description, url)
+                vacancies.append(vacancy)
+            return vacancies
+        else:
+            return f'Error: {response.status_code}'

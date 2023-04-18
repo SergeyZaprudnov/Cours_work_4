708fec44e689aac44a946b72e1e84e80c4d52d6a
sergey <szaprudnov@hotmail.com>
2023-04-18 23:28:58 +0300

    editing

--- heet/utils.py
+++ heet/utils.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 def user_interaction():
     """Общение\связь с пользователем"""
-    hh_api, sj_appi = choice_platform()
+    hh_api, sj_api = choice_platform()
     hh_vacancies, sj_vacancies = get_from_platform
     filter_word_input = filter_words()
     salary_input = salary_sort()
